a0e50161802ac53db330e25a7e20180ba87fc4e6
Игорь Надеин <gari.music126@yandex.ru>
2025-01-27 19:15:11 +0300

    made an early version of the models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,22 +8,47 @@ from django.db import models
 
 # Ресурс users: пользователи.
 
-# Ресурс titles: произведения, к которым пишут отзывы (определённый фильм, книга или песенка).
-class Title(models.Model):
-    pass
-
 # Ресурс categories: категории (типы) произведений («Фильмы», «Книги», «Музыка»).
 # Одно произведение может быть привязано только к одной категории.
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.slug
 
 # Ресурс genres: жанры произведений. Одно произведение может быть привязано к нескольким жанрам.
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.slug
+# Ресурс titles: произведения, к которым пишут отзывы (определённый фильм, книга или песенка).
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles', blank=True, null=True
+    )
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE
+    )
 
 # Ресурс reviews: отзывы на произведения. Отзыв привязан к определённому произведению.
 
