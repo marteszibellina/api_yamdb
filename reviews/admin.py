a0891845aa0d6c96befbb9d5c356fcb9a0467913
Игорь Надеин <gari.music126@yandex.ru>
2025-01-28 14:01:06 +0300

    the external folder was deleted 'reviews'

--- reviews/admin.py
+++
@@ -1,8 +0,0 @@
-"""reviews admin."""
-
-from django.contrib import admin
-
-from .models import Reviews, Comments
-
-admin.site.register(Reviews)
-admin.site.register(Comments)
--- reviews/consts.py
+++
@@ -1,4 +0,0 @@
-"""Константы приложения reviews."""
-
-MAX_TEXT_LENGTH = 5000
-MAX_COMMENT_LENGTH = 200
